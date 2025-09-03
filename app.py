43e17a1f5a1aaddcc4400dbf641dce8cfe204252
Admin <kimhaiareal@gmail.com>
2025-09-03 22:01:45 +0700

    add ui

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from pages.reduct.reduct import bp as reduct_bp
+from pages.k_means.k_means import bp as k_means_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(reduct_bp)
+blueprints = [reduct_bp, k_means_bp] 
+
+for bp in blueprints:
+    app.register_blueprint(bp)
 
 @app.route("/")
 def home():
